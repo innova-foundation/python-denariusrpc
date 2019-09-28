8712fdaa1ddd55d155d0b3a9c460ffb276ed1144
buzzkillb <travanx@gmail.com>
2019-09-27 19:24:54 -0700

    d init

--- jsonrpc/authproxy.py
+++ jsonrpc/authproxy.py
@@ -1,3 +1,3 @@
-from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+from denariusrpc.authproxy import AuthServiceProxy, JSONRPCException
 
 __all__ = ['AuthServiceProxy', 'JSONRPCException']
