5225340feb2b7ce5dfccab97c717acd397a2aecd
buzzkillb <travanx@gmail.com>
2019-09-27 19:32:59 -0700

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+from denariusrpc.authproxy import AuthServiceProxy, JSONRPCException
+
+# rpc_user and rpc_password are set in the denarius.conf file
+rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:32369"%("user", "password"))
+best_block_hash = rpc_connection.getbestblockhash()
+print(rpc_connection.getblock(best_block_hash))
+
+# batch support : print timestamps of blocks 0 to 99 in 2 RPC round-trips:
+commands = [ [ "getblockhash", height] for height in range(100) ]
+block_hashes = rpc_connection.batch_(commands)
+blocks = rpc_connection.batch_([ [ "getblock", h ] for h in block_hashes ])
+block_times = [ block["time"] for block in blocks ]
+print(block_times)
