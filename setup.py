669a15a6b15d48089b7b24f02431380b8d4bebab
buzzkillb <travanx@gmail.com>
2019-09-27 19:27:08 -0700

    Denarius

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 from distutils.core import setup
 
 setup(
-    name='python-bitcoinrpc',
+    name='python-denariusrpc',
     version='1.0',
-    description='Enhanced version of python-jsonrpc for use with Bitcoin',
+    description='Enhanced version of python-jsonrpc for use with Denarius',
     long_description=open('README.rst').read(),
-    author='Jeff Garzik',
-    author_email='<jgarzik@pobox.com>',
-    maintainer='Jeff Garzik',
+    author='buzzkillb',
+    author_email='<buzz@denarius.io>',
+    maintainer='buzzkillb',
     maintainer_email='<jgarzik@pobox.com>',
-    url='http://www.github.com/jgarzik/python-bitcoinrpc',
-    packages=['bitcoinrpc'],
+    url='http://www.github.com/buzzkillb/python-denariusrpc',
+    packages=['denariusrpc'],
     classifiers=[
         'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)', 'Operating System :: OS Independent'
     ]
