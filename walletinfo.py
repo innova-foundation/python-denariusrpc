a9750ded1b17ca85e0bbdb3edbe641f61072518f
buzzkillb <travanx@gmail.com>
2019-10-04 20:39:28 -0700

    denarius basic wallet info

---
+++ walletinfo.py
@@ -0,0 +1,16 @@
+from denariusrpc.authproxy import AuthServiceProxy, JSONRPCException
+
+# rpc_user and rpc_password are set in the denarius.conf file
+rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:32369"%("user", "password"))
+get_block_count = rpc_connection.getblockcount()
+print "Block Height:", (get_block_count)
+get_staking_info = rpc_connection.getstakinginfo()
+print "Wallet Staking Weight:", (get_staking_info[u'weight'])
+print "Net Stake Weight:", (get_staking_info[u'netstakeweight'])
+
+get_info = rpc_connection.getinfo()
+print "Peers:", (get_info[u'connections'])
+print "Daemon Version:", (get_info[u'version'])
+
+get_blockchain_info = rpc_connection.getblockchaininfo()
+print "Circulating:", (get_blockchain_info[u'moneysupply']), "D"
